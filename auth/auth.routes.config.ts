1d7ee08f0898528f440365e65162eb2cb91a5da6
Abner Grajales Concha <abner.grajales1991@gmail.com>
2021-06-18 00:31:00 -0500

    Refresh token

--- auth/auth.routes.config.ts
+++ auth/auth.routes.config.ts
@@ -1,6 +1,7 @@
 import { CommonRoutesConfig } from '../common/common.routes.config';
 import authController from './controllers/auth.controller';
 import authMiddleware from './middleware/auth.middleware';
+import jwtMiddleware from './middleware/jwt.middleware';
 import express from 'express';
 import BodyValidationMiddleware from '../common/middleware/body.validation.middleware';
 import { body } from 'express-validator';
@@ -16,6 +17,12 @@ export class AuthRoutes extends CommonRoutesConfig {
             body('password').isString(),
             BodyValidationMiddleware.verifyBodyFieldsErrors,
             authMiddleware.verifyUserPassword,
+            authController.createJWT,
+        ]);
+        this.app.post(`/auth/refresh-token`, [
+            jwtMiddleware.validJWTNeeded,
+            jwtMiddleware.verifyRefreshBodyField,
+            jwtMiddleware.validRefreshNeeded,
             authController.createJWT,
         ]);
 
